25c7889143fd4d78a05e9df770e903a48047cc51
yassa98 <yasirudeshan3@gmail.com>
2021-07-26 14:31:40 +0530

    fetch user profile and put likes functions created

--- frontend/src/components/posts/posts.jsx
+++ frontend/src/components/posts/posts.jsx
@@ -3,10 +3,20 @@ import "./posts.css";
 import { MoreVert} from "@material-ui/icons";
 import { useEffect,useState } from "react";
 import axios from 'axios';
+import { format} from "timeago.js";
 
 function Posts({post}) {
 
     const [user,setUser] = useState({});
+    const [like,setlike] = useState(post.likes.length);
+    const [isliked,setisLiked] = useState(false);
+
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+ 
+    const likehandler=()=>{
+        setlike(isliked ? like-1 : like+1);
+        setisLiked(!isliked);
+    }
 
     useEffect(()=>{
 
@@ -28,11 +38,13 @@ function Posts({post}) {
 
                     <div className="posttopleft">
 
-                        <img src={user.profilePicture} className="postprofileimg" alt=""/>
+                        <img src={user.profilePicture || PF+"noprofile.png"} 
+                             className="postprofileimg" 
+                             alt=""/>
                         <span className="postusername">
                             {user.username}
                         </span>
-                        <span className="postdate">{post.date}</span>
+                        <span className="postdate">{format(post.createdAt)}</span>
                     </div>
                     <div className="posttopright">
 
@@ -41,19 +53,20 @@ function Posts({post}) {
                 </div>
                 <div className="postcenter">
 
-                    <span className="postText">Hey it's my 1st post</span>
-                    <img className="postimg" src="assets/3.jpg" alt=""/>
+                    <span className="postText">{post.desc}</span>
+                    <img className="postimg" 
+                         src="assets/3.jpg" alt=""/>
                 </div>
 
                 <div className="postbottom">
                     <div className="postbottomleft">
-                        <img className="likeicon" src="assets/likes.jpg" alt=""/>
-                        <img className="hearticon" src="assets/heart.jpg" alt=""/>
-                        <img className="loveicon" src="assets/loves.jpg" alt=""/>
-                        <span className="postlikecounter">32 people liked this</span>
+                        <img className="likeicon" src="assets/likes.jpg" onClick={likehandler} alt=""/>
+                        <img className="hearticon" src="assets/heart.jpg" onClick={likehandler}alt=""/>
+                        <img className="loveicon" src="assets/loves.jpg" onClick={likehandler} alt=""/>
+                        <span className="postlikecounter">{like} people liked this</span>
                     </div>
                     <div className="postbottomright">
-                        <span className="postcommenttext">9 comments</span>
+                        <span className="postcommenttext">{post.comment} comments</span>
                     </div>
                 </div>
             </div>
